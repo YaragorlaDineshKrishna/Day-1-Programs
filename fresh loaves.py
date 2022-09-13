f0744b8b755c1ffeb3fde50eef09b7b9e89e664f
YaragorlaDineshKrishna <112241659+YaragorlaDineshKrishna@users.noreply.github.com>
2022-09-13 09:13:36 +0530

    Add files via upload

---
+++ fresh loaves.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number for reversing it:"))
+b=int(input("enter the nuber"))
+sum=0
+while(a>0):
+    z=a%10
+    sum=sum*10+z
+    a=a//10
+print("After reversing the number:",sum)
+print("after reversing the number:",sum)
